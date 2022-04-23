8069ac26a191bf95fbfc848f7eaf790b42d3fe43
杨裕炜 <yyw55535@sina.com>
2022-04-23 17:13:37 +0800

    ui

---
+++ pages/shops/detail.vue
@@ -0,0 +1,156 @@
+<template>
+	<view class="contain">
+		<view class="fixed-top flex h-44 bg-color-f3" :style="{'padding-top': top + 'px'}">
+			<view @click="back(1)" class="flex f-a-c f-j-c padding-lr12 van-icon van-icon-arrow-left f20-size"></view>
+			<view class="flex flex-1 f-a-c margin-r4">
+				<view class="flex flex-1 f-a-c h-32 bg-color-w b-radius-30 padding-lr10">
+					<text class="flex f-a-c van-icon van-icon-search f16-size margin-r4"></text>
+					<text class="t-color-9">搜索商品、类目</text>
+				</view>
+			</view>
+			<view class="flex f-a-c f-j-c padding-lr5 van-icon van-icon-share-o f20-size"></view>
+			<view class="flex f-a-c f-j-c padding-lr5 van-icon van-icon-bars f20-size"></view>
+			<view class="flex f-a-c f-j-c padding-lr5 van-icon van-icon-shopping-cart-o f20-size "></view>
+			<view class="padding-lr2"></view>
+			<!-- #ifdef MP-WEIXIN -->
+			<view class="flex f-s-0" style="width: 180upx;"></view>
+			<!-- #endif -->
+		</view>
+		<view :style="{'padding-top': top+44+'px'}"></view>
+		<view class="wrap-banner" >
+			<swiper class="wrap-banner" :indicator-dots="false" :autoplay="true" :interval="3000" :duration="1000">
+				<swiper-item >
+					<view class="bg-color h100 "></view>
+				</swiper-item>
+				<swiper-item>
+					<view class="bg-color-linear-y h100 "></view>
+				</swiper-item>
+			</swiper>
+			<view class="flex f-a-c f-j-c wrap-dot  b-radius-30 t-color-w f10-size">5/6</view>
+		</view>
+		
+		<view class="padding-12">
+			<view class="bg-color-w b-radius-10 padding-12">
+				<view class="flex ">
+					<view class="text-price f18-size f-w-b t-color-p margin-r8">18</view>
+					<view class="flex f-a-c f10-size t-color-9">
+						<text class="margin-r4">价格</text>
+						<text class="text-price del-line">155</text>
+					</view>
+				</view>
+				<view class="flex f-a-c f-j-s margin-t6">
+					<view class="flex f-a-c f-j-c h-16 b-radius-30 t-color-w bg-color-r f10-size padding-lr6">自营</view>
+					<view class="f10-size t-color-9">月销 5</view>
+				</view>
+				<view class="f12-size f-w-b margin-t6">针织衫春秋季新款女装毛衣2020春温柔风黑色长袖短款上衣开衫外套</view>
+			</view>
+			<view class="bg-color-w b-radius-10 padding-12 margin-t12">
+				<view class="flex padding-tb8">
+					<view class="flex f-s-0 f12-size t-color-9 margin-r10">选择</view>
+					<view class="flex f12-size flex-1 padding-lr10">颜色分类</view>
+					<view class="flex f-a-c f-s-0 van-icon f12-size van-icon-arrow t-color-9"></view>
+				</view>
+				<view class="flex padding-tb8">
+					<view class="flex f-s-0 f12-size t-color-9 margin-r10">发货</view>
+					<view class="flex f12-size flex-1 padding-lr10">
+						<text class="margin-r12">山东济宁</text>
+						<text class="margin-r12 t-color-9">|</text>
+						<text class="margin-r12">快递：免运费</text>
+					</view>
+					<view class="flex f-a-c f-s-0 van-icon f12-size van-icon-arrow t-color-9"></view>
+				</view>
+				<view class="flex padding-tb8">
+					<view class="flex f-s-0 f12-size t-color-9 margin-r10">参数</view>
+					<view class="flex f12-size flex-1 padding-lr10">颜色分类</view>
+					<view class="flex f-a-c f-s-0 van-icon f12-size van-icon-arrow t-color-9"></view>
+				</view>
+			</view>
+			
+			<view class="bg-color-w b-radius-10 padding-12 margin-t12">
+				<view class="flex f-a-c f-j-s">
+					<view class="f-w-b">宝贝评价(100+)</view>
+					<view class="flex f-a-c">
+						<text class="f10-size t-color-y margin-r4">查看全部</text>
+						<text class="flex f-a-c van-icon van-icon-arrow f10-size t-color-y"></text>
+					</view>
+				</view>
+				<view class="flex margin-t12">
+					<view class="flex w-30 h-30 b-radius bg-color margin-r12"></view>
+					<view class="flex f-c f-j-s">
+						<text class="f10-size">dsasdasa</text>
+						<text class="f10-size t-color-9">22天前</text>
+					</view>
+				</view>
+				<view class="f12-size margin-t10">查看全部查看全部查看全部查看全部查看全部查看全部</view>
+			</view>
+			
+			<view class="bg-color-w b-radius-10 padding-12 margin-t12">
+				<view class="flex">
+					<view class="flex w-50 h-50 b-radius bg-color margin-r12"></view>
+					<view class="flex f-c f-j-c">
+						<text class="f15-size f-w-b">前海万联旗舰店</text>
+						<text class="f12-size t-color-9 margin-t4">249 人关注店铺</text>
+					</view>
+				</view>
+			</view>
+			
+		</view>
+		<view class="flex f-a-c f-j-c no-more t-color-9 f12-size margin-t2">宝贝详情</view>
+		<view class="padding-tb6"></view>
+		<view  :style="{'padding-bottom': isIphonex ? '84px' : '50px'}"></view>
+		<view class="fixed-top bottom-btn bg-color-w flex padding-lr12 h-50" :style="{'padding-bottom': isIphonex ? '34px' : ''}">
+			<view class="flex flex-1 f-j-s margin-r20">
+				<view class="flex f-c f-a-c f-j-c">
+					<text class="flex f-a-c van-icon van-icon-shop-o f18-size t-color-y"></text>
+					<text class="f10-size t-color-9 margin-t2">店铺</text>
+				</view>
+				<view class="flex f-c f-a-c f-j-c">
+					<text class="flex f-a-c van-icon van-icon-more-o f18-size "></text>
+					<text class="f10-size t-color-9 margin-t2">客服</text>
+				</view>
+				<view class="flex f-c f-a-c f-j-c">
+					<text class="flex f-a-c van-icon van-icon-star-o f18-size "></text>
+					<text class="f10-size t-color-9 margin-t2">收藏</text>
+				</view>
+			</view>
+			<view class="flex f-a-c">
+				<view class="flex b-radius-30 h-34 over-h">
+					<view class="flex f-a-c f-j-c bg-color-linear-y t-color-w f12-size w-100">加入购物车</view>
+					<view class="flex f-a-c f-j-c bg-color-p t-color-w f12-size w-100">立即购买</view>
+				</view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+<style scoped>
+@import url('../../static/css/shops/detail.css');
+</style>
+<script>
+	const $ = require('../../utils/api.js');
+	let self;
+	export default {
+		data() {
+			return {
+				top: uni.getStorageSync('bartop') ? uni.getStorageSync('bartop') : 0,
+				isIphonex: uni.getStorageSync('isIphonex') ? uni.getStorageSync('isIphonex') : false,
+			};
+		},
+		onLoad: function() {
+			self = this;
+			this.init();
+		},
+		methods: {
+			init() {},
+		},
+		created() {
+		},
+		mounted() {},
+		destroyed() {},
+		components: {},
+		onPullDownRefresh() {
+		},
+		onReachBottom() {
+		}
+	}
+</script>
---
+++ pages/user/index.vue
@@ -0,0 +1,220 @@
+<template>
+	<view class="contain">
+		<view class="wrap-header">
+			<view class="flex f-j-e h-44 padding-lr5">
+				<view class="flex f-a-c f-j-c van-icon van-icon-qr f20-size t-color-3 padding-lr5"><text
+						class="f12-size">{{i18n['会员码']}}</text></view>
+				<view class="flex f-a-c f-j-c van-icon van-icon-setting-o f20-size padding-lr5"></view>
+				<view class="flex f-a-c f-j-c van-icon van-icon-chat-o f20-size padding-lr5"></view>
+			</view>
+			<view class="padding-lr15 flex">
+				<view class="w-60 h-60 b-radius bg-color bg-img margin-r12"></view>
+				<view class="flex f-c f-a-s">
+					<view class="f22-size">{{i18n['登录']}} / {{i18n['注册']}}</view>
+					<view class="f10-size padding-lr6 b-radius-2 h-16 t-color-w bg-color-linear-r margin-t6">
+						{{i18n['Hi，欢迎登录']}}</view>
+				</view>
+			</view>
+			<view class="flex margin-t15">
+				<view class="flex flex-1 f-c f-a-c f-j-c">
+					<text class="f16-size f-w-b">0</text>
+					<text class="f12-size">{{i18n['收藏夹']}}</text>
+				</view>
+				<view class="flex flex-1 f-c f-a-c f-j-c">
+					<text class="f16-size f-w-b">0</text>
+					<text class="f12-size">{{i18n['关注店铺']}}</text>
+				</view>
+				<view class="flex flex-1 f-c f-a-c f-j-c">
+					<text class="f16-size f-w-b">0</text>
+					<text class="f12-size">{{i18n['足迹']}}</text>
+				</view>
+				<view class="flex flex-1 f-c f-a-c f-j-c">
+					<text class="f16-size f-w-b">0</text>
+					<text class="f12-size">{{i18n['全部订单']}}</text>
+				</view>
+			</view>
+			<view class="padding-20"></view>
+			<view class="wrap-coupon-btn flex b-radius-5 bg-color-w padding-10">
+				<view class="flex flex-1 f-a-c f-j-s padding-lr2 wrap-coupon-btn-left margin-r10">
+					<view class="flex f-c">
+						<text class="f12-size f-w-b">{{i18n['红包卡券']}}</text>
+						<view class="flex f-a-c">
+							<text class="f10-size t-color-y margin-r4">{{i18n['领取优惠券']}}</text>
+							<text class="flex f-a-c van-icon van-icon-arrow t-color-y f12-size"></text>
+						</view>
+					</view>
+					<image src="../../static/images/user_coupon_icon.png" mode="widthFix" class="w-36 margin-r12">
+					</image>
+				</view>
+				<view class="flex flex-1 f-a-c f-j-s padding-lr2">
+					<view class="flex f-c">
+						<text class="f12-size f-w-b">{{i18n['我的积分']}}</text>
+						<view class="flex f-a-c">
+							<text class="f10-size t-color-y margin-r4">{{i18n['查看积分明细']}}</text>
+							<text class="flex f-a-c van-icon van-icon-arrow t-color-y f12-size"></text>
+						</view>
+					</view>
+					<image src="../../static/images/user_vip_icon.png" mode="widthFix" class="w-36 margin-r12"></image>
+				</view>
+			</view>
+		</view>
+		<view class="padding-12"></view>
+		<view class="padding-lr12">
+			<view class="bg-color-w b-radius-5  flex margin-t12 wrap-nav">
+				<view class="flex flex-1 f-s-0 f-c f-a-c f-j-c">
+					<text class="flex f-a-c van-icon van-icon-pending-payment f24-size"></text>
+					<text class="f12-size margin-t2">{{i18n['待支付']}}</text>
+				</view>
+				<view class="flex flex-1 f-s-0 f-c f-a-c f-j-c">
+					<text class="flex f-a-c van-icon van-icon-send-gift-o f24-size"></text>
+					<text class="f12-size margin-t2">{{i18n['待发货']}}</text>
+				</view>
+				<view class="flex flex-1 f-s-0 f-c f-a-c f-j-c">
+					<text class="flex f-a-c van-icon van-icon-logistics f24-size"></text>
+					<text class="f12-size margin-t2">{{i18n['待收货']}}</text>
+				</view>
+				<view class="flex flex-1 f-s-0 f-c f-a-c f-j-c">
+					<text class="flex f-a-c van-icon van-icon-comment-o f24-size"></text>
+					<text class="f12-size margin-t2">{{i18n['待评价']}}</text>
+				</view>
+				<view class="flex flex-1 f-s-0 f-c f-a-c f-j-c nav-last-item">
+					<text class="flex f-a-c van-icon van-icon-tosend f24-size t-color-y"></text>
+					<text class="f12-size margin-t2">{{i18n['退货/售后']}}</text>
+				</view>
+			</view>
+			<view class="bg-color-w b-radius-5  flex margin-t12 wrap-nav">
+				<view class="flex flex-1 f-s-0 f-c f-a-c f-j-c">
+					<text class="f16-size f-w-b">0</text>
+					<text class="f12-size margin-t6">{{i18n['我的卡券']}}</text>
+				</view>
+				<view class="flex flex-1 f-s-0 f-c f-a-c f-j-c">
+					<text class="f16-size f-w-b">0</text>
+					<text class="f12-size margin-t6">{{i18n['余额']}}</text>
+				</view>
+				<view class="flex flex-1 f-s-0 f-c f-a-c f-j-c">
+					<text class="f16-size f-w-b">0</text>
+					<text class="f12-size margin-t6">{{i18n['银行卡']}}</text>
+				</view>
+				<view class="flex flex-1 f-s-0 f-c f-a-c f-j-c">
+					<text class="f16-size f-w-b">0</text>
+					<text class="f12-size margin-t6">{{i18n['积分']}}</text>
+				</view>
+				<view class="flex flex-1 f-s-0 f-c f-a-c f-j-c nav-last-item">
+					<text class="flex f-a-c van-icon van-icon-bill-o f24-size t-color-y"></text>
+					<text class="f12-size margin-t2">{{i18n['账单明细']}}</text>
+				</view>
+			</view>
+			<view class="bg-color-w b-radius-5  flex f-w margin-t12" style="padding-top: 12px;">
+				<view class="flex f-s-0 f-c btn-item">
+					<view class="flex f-a-c f-j-c h-30">
+						<text class="flex f-a-c f-j-c shops-icon shops-icon-qianbao f26-size"></text>
+					</view>
+					<view class="flex f-j-c f12-size t-color-6 margin-t2">{{i18n['钱包']}}</view>
+				</view>
+				<view class="flex f-s-0 f-c btn-item">
+					<view class="flex f-a-c f-j-c h-30">
+						<text class="flex f-a-c f-j-c shops-icon shops-icon-pintuan f24-size"></text>
+					</view>
+					<view class="flex f-j-c f12-size t-color-6 margin-t2">{{i18n['拼团订单']}}</view>
+				</view>
+				<view class="flex f-s-0 f-c btn-item">
+					<view class="flex f-a-c f-j-c h-30">
+						<text class="flex f-a-c f-j-c shops-icon shops-icon-pinglun-08 f24-size"></text>
+					</view>
+					<view class="flex f-j-c f12-size t-color-6 margin-t2">{{i18n['评论']}}</view>
+				</view>
+				<view class="flex f-s-0 f-c btn-item">
+					<view class="flex f-a-c f-j-c h-30">
+						<text class="flex f-a-c f-j-c shops-icon shops-icon-location1 f26-size"></text>
+					</view>
+					<view class="flex f-j-c f12-size t-color-6 margin-t2">{{i18n['收货地址']}}</view>
+				</view>
+				<view class="flex f-s-0 f-c btn-item">
+					<view class="flex f-a-c f-j-c h-30">
+						<text class="flex f-a-c f-j-c shops-icon shops-icon-qiandao f24-size"></text>
+					</view>
+					<view class="flex f-j-c f12-size t-color-6 margin-t2">{{i18n['签到']}}</view>
+				</view>
+				<view class="flex f-s-0 f-c btn-item">
+					<view class="flex f-a-c f-j-c h-30">
+						<text class="flex f-a-c f-j-c shops-icon shops-icon-gerenjubaodengji f24-size"></text>
+					</view>
+					<view class="flex f-j-c f12-size t-color-6 margin-t2">{{i18n['我的举报']}}</view>
+				</view>
+				<view class="flex f-s-0 f-c btn-item">
+					<view class="flex f-a-c f-j-c h-30">
+						<text class="flex f-a-c f-j-c shops-icon shops-icon-yijianfankui f28-size"></text>
+					</view>
+					<view class="flex f-j-c f12-size t-color-6 margin-t2">{{i18n['反馈']}}</view>
+				</view>
+				<view class="flex f-s-0 f-c btn-item">
+					<view class="flex f-a-c f-j-c h-30">
+						<text class="flex f-a-c f-j-c shops-icon shops-icon-bangzhu f26-size"></text>
+					</view>
+					<view class="flex f-j-c f12-size t-color-6 margin-t2">{{i18n['帮助中心']}}</view>
+				</view>
+				<view class="flex f-s-0 f-c btn-item">
+					<view class="flex f-a-c f-j-c h-30">
+						<text class="flex f-a-c f-j-c shops-icon shops-icon-kefu f24-size"></text>
+					</view>
+					<view class="flex f-j-c f12-size t-color-6 margin-t2">{{i18n['智能小蜜']}}</view>
+				</view>
+				<view class="flex f-s-0 f-c btn-item">
+					<view class="flex f-a-c f-j-c h-30">
+						<text class="flex f-a-c f-j-c shops-icon shops-icon-faxian f28-size"></text>
+					</view>
+					<view class="flex f-j-c f12-size t-color-6 margin-t2">{{i18n['创作中心']}}</view>
+				</view>
+				<view class="flex f-s-0 f-c btn-item">
+					<view class="flex f-a-c f-j-c h-30">
+						<text class="flex f-a-c f-j-c shops-icon shops-icon-shezhi f24-size"></text>
+					</view>
+					<view class="flex f-j-c f12-size t-color-6 margin-t2">{{i18n['设置']}}</view>
+				</view>
+			</view>
+			<view class="flex f-a-c f-j-c margin-t12">
+				<image class="h-18" src="../../static/images/guess_you_like_it.png" mode="heightFix"></image>
+			</view>
+			<goodslist class="margin-t12" :list="list"></goodslist>
+		</view>
+		<tab-bar :active="4"></tab-bar>
+	</view>
+</template>
+<style scoped>
+	@import url('../../static/css/user/index.css');
+	@import url('../../static/css/iconcolor.css');
+</style>
+<script>
+	const $ = require('../../utils/api.js');
+	let self;
+	export default {
+		data() {
+			return {
+				list: [{
+					id: 6,
+					image_url: "https://stc.wanlshop.com/1a42d31bbb0f1d8d9aa66f804b47c7a3.jpg?x-oss-process=image/auto-orient,1/interlace,1/format,jpg/quality,q_90/sharpen,50/resize,m_mfit,w_516",
+					title: "恐龙",
+					text: "恐龙来啦",
+				}, ],
+			};
+		},
+		onLoad: function() {
+			self = this;
+			this.init();
+		},
+		methods: {
+			init() {},
+		},
+		created() {},
+		computed: {
+			i18n() {
+				return this.$t('user')
+			},
+		},
+		mounted() {},
+		destroyed() {},
+		components: {},
+		onPullDownRefresh() {},
+		onReachBottom() {}
+	}
+</script>
