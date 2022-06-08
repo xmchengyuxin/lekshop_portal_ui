5f19ecc6a8831843b5a99ce9c3379146621202e7
杨裕炜 <297786857@qq.com>
2022-06-08 10:31:42 +0800

    add

--- utils/app.js
+++ utils/app.js
@@ -1,11 +1,11 @@
 export default {
 	'daxiang': {
 		name: '商城',
-		// api: 'https://lekportalapi.chengyuxin.net/',
-		api: 'http://192.168.0.106:7076/',
+		api: 'https://lekportalapi.chengyuxin.net/',
+		// api: 'http://192.168.0.106:7076/',
 		hot: 'https://h5.wode24h.com/hotpush_user.wgt',
-		// socket: 'ws://101.34.165.178:8008/ws',
-		socket: 'ws://192.168.0.106:8008/ws',
+		socket: 'ws://101.34.165.178:8008/ws',
+		// socket: 'ws://192.168.0.106:8008/ws',
 		version: '10000'
 	},
 }
